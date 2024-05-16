fb8a509a350ac56147b4fb29797871f24bcc4960
Shreejanlama <shreejanlama0@gmail.com>
2024-05-16 17:24:03 +0545

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,6 +13,7 @@ const Navbar = () => {
         <ul className="flex gap-6">
           <li className="font-bold text-blue-900 hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl">Contact Us</li>
           <li className="font-bold text-blue-900 hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl">About</li>
+          <li className="font-bold text-blue-900 hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl">Trainer</li>
           <li className="font-bold text-blue-900 hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl">Popular Courses</li>
           <li className="font-bold text-blue-900 hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl">Blogs</li>
         </ul>
