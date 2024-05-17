be3fd2a1eda06c3b1a12f77cf2f6b99c22dc483c
ngimatendi <ngimatendi12@gmail.com>
2024-05-17 15:15:43 +0545

    Footers work is done and Also Course list is linked in the Navbar

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Contact from '@/components/Contact'
+import Footer from '@/components/Footer';
 
 const contact = () => {
   return (
-    <div> <Contact /></div>
+    <div>
+      <Contact />
+      <Footer />
+    </div>
   )
 }
 
