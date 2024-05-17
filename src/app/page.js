233bccbcb99be5467e89bc248ab8e0f6a4783aeb
ngimatendi <ngimatendi12@gmail.com>
2024-05-17 15:15:43 +0545

    Footers work is done and Also Course list is linked in the Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Hero from "../components/Hero";
 import CourseList from "../components/CourseList";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <div>
       <Hero />
       <CourseList />
+      <Footer />
     </div>
   );
 }
-
