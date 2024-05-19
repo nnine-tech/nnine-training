d5eb9656a42c0ab2c0d86b7376cc8b65205cbc27
Mingtindu <mingmartu@gmail.com>
2024-05-19 08:05:14 +0545

    feat: markdown for courselist added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,33 @@
+// pages/index.js
+"use client"; // This marks the file as a client component
+
+import { useEffect, useState } from 'react';
 import Hero from "../components/Hero";
 import CourseList from "../components/CourseList";
 import Footer from "@/components/Footer";
+import { fetchMarkdownFile } from '../utils/fetchMarkdown';
+
 export default function Home() {
+  const [courseData, setCourseData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchMarkdownFile('/markdown/courselist.md');
+        setCourseData(data);
+      } catch (error) {
+        console.error("Error fetching markdown file:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(courseData); // This should now show the data
+
   return (
     <div>
       <Hero />
-      <CourseList />
+      <CourseList courseData={courseData} />
       <Footer />
     </div>
   );
