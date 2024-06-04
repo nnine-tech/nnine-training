cb7fc6e4ae29cdbc753fd08cdfd0e9d2227e3865
Lhachhen12 <shreejanlama608@gmail.com>
2024-06-04 19:11:48 +0545

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -89,6 +89,14 @@ const NavBar = () => {
             </li>
             <li>
               <Link
+                href="Career"
+                className="font-bold text-blue-900 hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl"
+              >
+                Career
+              </Link>
+            </li>
+            <li>
+              <Link
                 href="/About"
                 className="font-bold text-blue-900 hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl"
               >
