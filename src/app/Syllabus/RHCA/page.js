0edd5af32e29c673c98fb249650fe73ff267cc11
Lhachhen12 <shreejanlama608@gmail.com>
2024-06-07 11:47:46 +0545

    Feat: New syllabus add

---
+++ src/app/Syllabus/RHCA/page.js
@@ -0,0 +1,12 @@
+import RHCASyllabus from "@/Syllabus/Rhca";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <RHCASyllabus />
+    </div>
+  );
+};
+
+export default page;
