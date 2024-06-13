a01e4a1d9085f7d43658fd2b0a99a4a7d02638b0
Lhachhen12 <shreejanlama608@gmail.com>
2024-06-13 15:51:18 +0545

    Feat: New jobtitle add

---
+++ src/components/Digitalmarketing.js
@@ -0,0 +1,99 @@
+import React from "react";
+
+const DigitalMarketing = () => {
+  return (
+    <div className="mt-4 p-4 border rounded-lg shadow-md">
+      <h2 className="font-semibold text-xl mb-2">Job Description</h2>
+
+      <h3 className="font-semibold text-lg mb-2">About Us</h3>
+      <p className="mb-4">
+        N9 Solution is a pioneering IT training and service provider committed
+        to delivering top-notch educational and technological solutions.
+        Renowned for our innovative approach and unparalleled customer service,
+        we are dedicated to shaping the future of digital education.
+      </p>
+
+      <h3 className="font-semibold text-lg mb-2">Position Overview</h3>
+      <p className="mb-4">
+        We're in search of a Digital Marketing Trainer to join our esteemed team
+        and spearhead the journey of equipping budding marketers with essential
+        skills. As a Digital Marketing Teacher at N9 Solution Pvt. Ltd., you'll
+        play a pivotal role in inspiring and guiding learners as they navigate
+        the dynamic realm of digital marketing.
+      </p>
+
+      <h3 className="font-semibold text-lg mb-2">Responsibilities:</h3>
+      <ul className="list-disc list-inside mb-4">
+        <li>
+          Collaborate closely with learners to craft engaging and comprehensive
+          learning experiences in digital marketing.
+        </li>
+        <li>
+          Customize teaching methodologies and educational materials to cater to
+          diverse learning styles and proficiency levels.
+        </li>
+        <li>
+          Reinforce theoretical concepts with real-world examples and
+          interactive exercises to deepen understanding and practical
+          application.
+        </li>
+        <li>
+          Guide learners in implementing digital marketing strategies through
+          hands-on projects, nurturing a thorough comprehension and skill
+          mastery.
+        </li>
+        <li>
+          Stay updated with the latest trends and best practices in digital
+          marketing, seamlessly integrating relevant content into teaching
+          materials and sessions.
+        </li>
+        <li>
+          Contribute actively to curriculum development and assessment endeavors
+          to ensure alignment with industry standards and learning objectives.
+        </li>
+        <li>
+          Foster an inclusive and supportive learning environment that
+          encourages collaboration and knowledge exchange among learners.
+        </li>
+      </ul>
+
+      <h3 className="font-semibold text-lg mb-2">Requirements:</h3>
+      <ul className="list-disc list-inside mb-4">
+        <li>
+          Bachelor's degree in Marketing, Communications, Business
+          Administration, or any related field.
+        </li>
+        <li>
+          Proficiency in digital marketing tools, platforms, and strategies.
+        </li>
+        <li>
+          Experience in teaching or training digital marketing concepts is
+          highly desirable.
+        </li>
+        <li>
+          Exceptional communication, presentation, and interpersonal skills.
+        </li>
+        <li>
+          Adaptability and flexibility in tailoring teaching strategies to meet
+          the diverse needs of learners.
+        </li>
+      </ul>
+
+      <h3 className="font-semibold text-lg mb-2">How to Apply:</h3>
+      <p className="mb-4">
+        Are you ready to make a lasting impact on the next generation of
+        marketers? Send your updated CV to
+        <a
+          href="mailto:nninesolution@gmail.com"
+          style={{ textDecoration: "underline", color: "blue" }}
+        >
+          nninesolution@gmail.com
+        </a>{" "}
+        and embark on a journey of empowering aspiring digital marketers with
+        us!
+      </p>
+    </div>
+  );
+};
+
+export default DigitalMarketing;
---
+++ src/components/graphics.js
@@ -0,0 +1,95 @@
+import React from "react";
+
+const GraphicsDesign = () => {
+  return (
+    <div className="mt-4 p-4 border rounded-lg shadow-md">
+      <h2 className="font-semibold text-xl mb-2">Job Description</h2>
+
+      <h3 className="font-semibold text-lg mb-2">About Us</h3>
+      <p className="mb-4">
+        N9 Solution is a premier IT training and service provider committed to
+        delivering top-quality educational and technological solutions. Renowned
+        for our innovative approach and exceptional customer service, we are
+        dedicated to shaping the future of digital creativity.
+      </p>
+
+      <h3 className="font-semibold text-lg mb-2">Position Overview</h3>
+      <p className="mb-4">
+        We're seeking a Graphics Design Trainer to join our dynamic team and
+        lead the way in imparting essential skills to aspiring designers. As a
+        Graphics Design Teacher at N9 Solution Pvt. Ltd., you'll have the
+        opportunity to inspire and mentor learners as they explore the dynamic
+        world of graphic design.
+      </p>
+
+      <h3 className="font-semibold text-lg mb-2">Responsibilities:</h3>
+      <ul className="list-disc list-inside mb-4">
+        <li>
+          Collaborate closely with learners to develop engaging and
+          comprehensive learning experiences in graphics design.
+        </li>
+        <li>
+          Customize teaching methodologies and instructional materials to cater
+          to diverse learning styles and proficiency levels.
+        </li>
+        <li>
+          Utilize real-world examples and hands-on exercises to reinforce
+          concepts and enhance understanding of graphic design principles.
+        </li>
+        <li>
+          Guide learners in applying graphic design principles to practical
+          projects, fostering a deeper level of comprehension and skill mastery.
+        </li>
+        <li>
+          Stay updated with the latest trends and best practices in graphic
+          design, seamlessly integrating relevant content into teaching
+          materials and sessions.
+        </li>
+        <li>
+          Contribute actively to curriculum development and assessment
+          activities to ensure alignment with industry standards and learning
+          objectives.
+        </li>
+        <li>
+          Foster a supportive and inclusive learning environment that encourages
+          collaboration and knowledge sharing among learners.
+        </li>
+      </ul>
+
+      <h3 className="font-semibold text-lg mb-2">Requirements:</h3>
+      <ul className="list-disc list-inside mb-4">
+        <li>Bachelor's degree in Computer Sceince, or any related field.</li>
+        <li>
+          Proficiency in graphic design software such as Adobe Creative Suite
+          (Photoshop, Illustrator, InDesign, etc.).
+        </li>
+        <li>
+          Experience in teaching or training graphic design concepts is
+          preferred.
+        </li>
+        <li>
+          Excellent communication, presentation, and interpersonal skills.
+        </li>
+        <li>
+          Adaptability and flexibility in adjusting teaching strategies to meet
+          the needs of diverse learners.
+        </li>
+      </ul>
+
+      <h3 className="font-semibold text-lg mb-2">How to Apply:</h3>
+      <p className="mb-4">
+        Are you ready to make a significant impact on the next generation of
+        graphic designers? Send your updated CV to{" "}
+        <a
+          href="mailto:nninesolution@gmail.com"
+          style={{ textDecoration: "underline", color: "blue" }}
+        >
+          nninesolution@gmail.com
+        </a>{" "}
+        and join us in empowering aspiring graphic design enthusiasts with us!
+      </p>
+    </div>
+  );
+};
+
+export default GraphicsDesign;
