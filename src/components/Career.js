0ac251f1fd6ef665b1ba322d8196af565e01e110
Lhachhen12 <shreejanlama608@gmail.com>
2024-06-13 15:51:18 +0545

    Feat: New jobtitle add

--- src/components/Career.js
+++ src/components/Career.js
@@ -1,49 +1,181 @@
 "use client";
-import { useState } from 'react';
-import { ChevronDownIcon } from '@heroicons/react/24/solid';
-import JobDescription from '@/components/Description';
+import { useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import SoftwareQualityAssurance from "@/components/SQA";
+import DigitalMarketing from "@/components/Digitalmarketing";
+import GraphicsDesign from "@/components/graphics";
+import PythonWithDjango from "@/components/Python&django";
+import JobDescription from "@/components/Description";
+
+const jobData = [
+  {
+    title: "Software Quality Assurance",
+    company: "Nnine Solution Private Limited",
+    jobexpire: "",
+    location: "Chabahil-Kathmandu",
+    jobType: "Part Time",
+    openingDate: "2081-02-30",
+    endDate: "2081-03-10",
+    isExpired: false,
+    position: "Trainer",
+    descriptionComponent: SoftwareQualityAssurance,
+  },
+  {
+    title: "Digital Marketing",
+    company: "Nnine Solution Private Limited",
+    jobexpire: "",
+    location: "Chabahil-Kathmandu",
+    jobType: "Part Time",
+    openingDate: "2081-02-30",
+    endDate: "2081-03-10",
+    isExpired: false,
+    position: "Trainer",
+    descriptionComponent: DigitalMarketing,
+  },
+  {
+    title: "Graphics Design",
+    company: "Nnine Solution Private Limited",
+    jobexpire: "",
+    location: "Chabahil-Kathmandu",
+    jobType: "Part Time",
+    openingDate: "2081-02-30",
+    endDate: "2081-03-10",
+    isExpired: false,
+    position: "Trainer",
+    descriptionComponent: GraphicsDesign,
+  },
+  {
+    title: "Python with Django",
+    company: "Nnine Solution Private Limited",
+    jobexpire: "",
+    location: "Chabahil-Kathmandu",
+    jobType: "Part Time",
+    openingDate: "2081-02-30",
+    endDate: "2081-03-10",
+    isExpired: false,
+    position: "Trainer",
+    descriptionComponent: PythonWithDjango,
+  },
+  {
+    title: "Front Desk Officer",
+    company: "Nnine Solution Private Limited",
+    jobexpire: "Closed",
+    location: "Chabahil-Kathmandu",
+    jobType: "Full Time",
+    openingDate: "2024-06-01",
+    endDate: "2024-06-08",
+    isExpired: true,
+    position: "FronDesk Officer",
+    descriptionComponent: JobDescription,
+  },
+];
 
 const JobTitle = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [openJob, setOpenJob] = useState(null);
 
-  const toggleDescription = () => {
-    setIsOpen(!isOpen);
+  const toggleDescription = (title) => {
+    setOpenJob(openJob === title ? null : title);
   };
 
   return (
-    <>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <div className="max-w-2xl mx-auto p-4">
-        <div 
-          className="cursor-pointer border rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow duration-300"
-          onClick={toggleDescription}
+    <div className="max-w-2xl mx-auto p-4 pt-[150px]">
+      {jobData.map((job) => (
+        <div
+          key={job.title}
+          className={`cursor-pointer border rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow duration-300 my-4 ${
+            job.isExpired && job.title !== "Front Desk Officer"
+              ? "bg-red-100"
+              : ""
+          }`}
+          onClick={() => toggleDescription(job.title)}
         >
           <div className="flex items-center justify-between">
             <div>
-              <h2 className="text-2xl font-bold text-blue-600">Job Title</h2>
-              <p>Nnine Solution Private Limited, Chabahil-Kathmandu</p>
+              <h2
+                className={`text-2xl font-bold ${
+                  job.isExpired ? "text-red-600" : "text-blue-600"
+                }`}
+              >
+                {job.title}
+              </h2>
+              {job.jobexpire && (
+                <p
+                  className={`${
+                    openJob === job.title ? "text-green-600" : "text-red-600"
+                  } font-bold`}
+                >
+                  {job.jobexpire}
+                </p>
+              )}
+              <p
+                className={
+                  job.isExpired && job.title !== "Front Desk Officer"
+                    ? "text-red-600 font-bold"
+                    : ""
+                }
+              >
+                {job.company}, {job.location}
+              </p>
               <div className="flex space-x-2 mt-2">
-                <span className="bg-blue-100 text-blue-800 px-2 py-1 rounded">Front Desk Officer</span>
-                <span className="bg-blue-100 text-blue-800 px-2 py-1 rounded">Full Time</span>
-                <span className="bg-blue-100 text-blue-800 px-2 py-1 rounded">Opening Date: 2024-06-01</span>
-                <span className="bg-blue-100 text-blue-800 px-2 py-1 rounded">End Date: 2024-06-08</span>
-                
+                <span
+                  className={`${
+                    job.isExpired
+                      ? job.title === "Front Desk Officer"
+                        ? "bg-blue-100 text-blue-800"
+                        : "bg-red-100 text-red-800"
+                      : "bg-blue-100 text-blue-800"
+                  } px-2 py-1 rounded`}
+                >
+                  {job.position}
+                </span>
+                <span
+                  className={`${
+                    job.isExpired
+                      ? job.title === "Front Desk Officer"
+                        ? "bg-blue-100 text-blue-800"
+                        : "bg-red-100 text-red-800"
+                      : "bg-blue-100 text-blue-800"
+                  } px-2 py-1 rounded`}
+                >
+                  {job.jobType}
+                </span>
+                <span
+                  className={`${
+                    job.isExpired
+                      ? job.title === "Front Desk Officer"
+                        ? "bg-blue-100 text-blue-800"
+                        : "bg-red-100 text-red-800"
+                      : "bg-blue-100 text-blue-800"
+                  } px-2 py-1 rounded`}
+                >
+                  Opening Date: {job.openingDate}
+                </span>
+                <span
+                  className={`${
+                    job.isExpired
+                      ? job.title === "Front Desk Officer"
+                        ? "bg-blue-100 text-blue-800"
+                        : "bg-red-100 text-red-800"
+                      : "bg-blue-100 text-blue-800"
+                  } px-2 py-1 rounded`}
+                >
+                  End Date: {job.endDate}
+                </span>
               </div>
               <div className="flex items-center mt-2">
                 <span className="text-blue-600 font-bold">Details </span>
-                <ChevronDownIcon className={`w-5 h-5 ml-1 transition-transform duration-300 ${isOpen ? 'transform rotate-180' : ''}`} />
+                <ChevronDownIcon
+                  className={`w-5 h-5 ml-1 transition-transform duration-300 ${
+                    openJob === job.title ? "transform rotate-180" : ""
+                  }`}
+                />
               </div>
             </div>
           </div>
+          {openJob === job.title && <job.descriptionComponent />}
         </div>
-        {isOpen && <JobDescription />}
-      </div>
-    </>
+      ))}
+    </div>
   );
 };
 
---
+++ src/components/Python&django.js
@@ -0,0 +1,96 @@
+import React from "react";
+
+const PythonWithDjango = () => {
+  return (
+    <div className="mt-4 p-4 border rounded-lg shadow-md">
+      <h2 className="font-semibold text-xl mb-2">Job Description</h2>
+
+      <h3 className="font-semibold text-lg mb-2">About Us</h3>
+      <p className="mb-4">
+        N9 Solution is a leading IT training and service provider, dedicated to
+        delivering high-quality educational and technological solutions. We
+        pride ourselves on our innovative approach and exceptional customer
+        service.
+      </p>
+
+      <h3 className="font-semibold text-lg mb-2">Position Overview</h3>
+      <p className="mb-4">
+        We're searching for a Python & Django Trainer to join our team and lead
+        the charge in imparting essential skills to aspiring coders. As a Python
+        Django Teacher at Nnine solution PVT. LTD, you'll have the opportunity
+        to inspire and mentor learners as they navigate the dynamic world of
+        Python programming and Django web development.
+      </p>
+
+      <h3 className="font-semibold text-lg mb-2">Responsibilities:</h3>
+      <ul className="list-disc list-inside mb-4">
+        <li>
+          Collaborate with learners to develop engaging and comprehensive
+          learning experiences in Python programming and Django web development.
+        </li>
+        <li>
+          Customize teaching methodologies and materials to accommodate various
+          learning styles and levels of expertise.
+        </li>
+        <li>
+          Apply real-world examples and hands-on exercises to reinforce concepts
+          and enhance understanding.
+        </li>
+        <li>
+          Guide learners in applying Python and Django principles to practical
+          projects, fostering a deeper level of comprehension and skill mastery.
+        </li>
+        <li>
+          Stay abreast of the latest advancements and best practices in Python
+          and Django, integrating relevant content into teaching materials and
+          sessions.
+        </li>
+        <li>
+          Participate in curriculum development and assessment activities to
+          ensure alignment with industry standards and learning objectives.
+        </li>
+        <li>
+          Cultivate a supportive and inclusive learning environment by
+          encouraging collaboration and knowledge sharing among learners.
+        </li>
+      </ul>
+
+      <h3 className="font-semibold text-lg mb-2">Requirements:</h3>
+      <ul className="list-disc list-inside mb-4">
+        <li>
+          Bachelor's degree in Computer Science, Software Engineering, or any
+          related field.
+        </li>
+        <li>
+          Proficiency in Python programming language and Django web framework.
+        </li>
+        <li>
+          Experience in teaching or training Python and Django concepts is
+          preferred.
+        </li>
+        <li>
+          Excellent communication, presentation, and interpersonal skills.
+        </li>
+        <li>
+          Adaptability and flexibility in adjusting teaching strategies to meet
+          the needs of diverse learners.
+        </li>
+      </ul>
+
+      <h3 className="font-semibold text-lg mb-2">How to Apply:</h3>
+      <p className="mb-4">
+        Ready to make a difference in the lives of aspiring developers? Send
+        your latest CV to{" "}
+        <a
+          href="mailto:nninesolution@gmail.com"
+          style={{ textDecoration: "underline", color: "blue" }}
+        >
+          nninesolution@gmail.com
+        </a>{" "}
+        and join us in empowering the next generation of coding enthusiasts!
+      </p>
+    </div>
+  );
+};
+
+export default PythonWithDjango;
