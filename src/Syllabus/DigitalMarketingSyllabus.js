05fd81de5877e5239318f9f1a23c85e0840589c1
ngimatendi <ngimatendi12@gmail.com>
2024-06-20 00:02:04 +0545

    feat: Digital Marketing, Graphic Designing and UIUX Syllabus Updated

--- src/Syllabus/DigitalMarketingSyllabus.js
+++ src/Syllabus/DigitalMarketingSyllabus.js
@@ -1,78 +1,198 @@
 import React from "react";
 
-const DigitalMarketingSyllabus = () => {
+const DataAnalytics = () => {
   return (
     <>
+      <br />
+      <br />
       <br />
       <br />
       <br />
       <div className="bg-blue-300 min-h-screen p-8">
         <div className="max-w-3xl mx-auto bg-white p-8 rounded-lg shadow-md">
           <div>
-            <h1 className="text-blue-500 text-3xl font-bold mb-4">
-              Digital Marketing Syllabus
+            <h1 className="text-blue-500 text-3xl font-bold mb-4 py-8">
+              Digital Marketing
             </h1>
 
-            <h2 className="text-blue-500 font-bold mb-4">
-              Digital Marketing Basics
-            </h2>
-            <ul className="list-disc ml-8">
-              <li>Principles of digital marketing</li>
-              <li>Latest Digital Marketing Trends</li>
-            </ul>
+            <h2 className="text-blue-500 font-bold mb-4">COURSE OUTLINE</h2>
 
             <h2 className="text-blue-500 font-bold mb-4">
-              Search Engine Optimization
+              Week 1: Introduction to Digital Marketing & Content Marketing
+              Strategy
             </h2>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 1: What is Digital Marketing?
+            </h3>
             <ul className="list-disc ml-8">
-              <li>SEM (Search Engine Marketing)</li>
-              <li>Search Engine Marketing Principles & Overview</li>
-              <li>Understanding Google Ads (Previously Adwords) Tool</li>
-              <li>Campaign Management</li>
-              <li>Ad writing / Creation Techniques</li>
-              <li>Understanding of Ad Targeting & Remarketing</li>
-              <li>Understanding of Landing Pages</li>
-              <li>Understanding of Conversion Tracking</li>
+              <li>Purpose of Digital Marketing</li>
+              <li>Digital Marketing Funnel</li>
+              <li>Unique Selling Proposition (USP)</li>
+              <li>Customer Acquisition</li>
             </ul>
 
-            <h2 className="text-blue-500 font-bold mb-4">
-              Social Media Marketing
-            </h2>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 2: What is Content Marketing?
+            </h3>
             <ul className="list-disc ml-8">
-              <li>Social Media Marketing Principles & Overview</li>
-              <li>Understanding of Core Optimization</li>
-              <li>Understanding of Content Development & Marketing</li>
-              <li>Understanding of Social Media Advertisement</li>
-              <li>Facebook / Instagram Marketing</li>
-              <li>Twitter Marketing</li>
-              <li>Linkedin Marketing</li>
-              <li>Youtube Marketing</li>
+              <li>Methods of Effective Content Marketing</li>
+              <li>Content Marketing Terminology</li>
+              <li>Developing a Content Marketing Plan</li>
+              <li>Types of Content Marketing</li>
+              <li>Content Mapping Based on Customer Journey</li>
+              <li>Marketing Campaigns: Inbound vs. Digital Marketing</li>
+              <li>B2B vs. B2C Digital Marketing Strategies</li>
             </ul>
 
-            <h2 className="text-blue-500 font-bold mb-4">Email Marketing</h2>
+            <h2 className="text-blue-500 font-bold py-4">
+              Week 2: Crafting a Digital Advertising Plan & Brand Building
+            </h2>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 1: Paid vs. Organic Traffic
+            </h3>
             <ul className="list-disc ml-8">
-              <li>Email Marketing Principles & Overview</li>
-              <li>Understanding of Lead Email Generation Methods</li>
-              <li>Understanding of Bulk Email Tools</li>
-              <li>Understanding of Frequency & Measurement</li>
+              <li>Key Metrics in Digital Advertising</li>
+              <li>Top Sources of Paid Traffic</li>
+              <li>Elements of Successful Digital Campaigns</li>
+              <li>Digital Advertising Terminology</li>
             </ul>
 
-            <h2 className="text-blue-500 font-bold mb-4">
-              Web Analytics & Conversion Rate Optimization
-            </h2>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 2: Introduction to SEO
+            </h3>
             <ul className="list-disc ml-8">
-              <li>Web Analytics Principles & Overview</li>
-              <li>Understanding & Configuration of Google Analytics</li>
-              <li>Understanding of Key Metrics & Dimensions</li>
-              <li>Learning of Event tracking</li>
-              <li>Learning of Goals set up & funnel visualization</li>
-              <li>Understanding & Configuration of Google Optimize</li>
+              <li>Importance of SEO for Business</li>
+              <li>Types of SEO</li>
+              <li>SEO Process and Pre-SEO Checklist</li>
+              <li>Site Architecture, Sitemap, and Keyword Analysis</li>
+              <li>Technical SEO, On-Page SEO, Off-Page SEO</li>
+            </ul>
+
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 3: Brand Building and Identity
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Basics of Branding</li>
+              <li>Building Brand Awareness in 2022</li>
+              <li>Importance of Brand Building</li>
+            </ul>
+
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 4: Google Analytics
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Understanding Audience Analytics</li>
+              <li>User Acquisition and Behavior</li>
+              <li>Setting Goals with Google Analytics</li>
+            </ul>
+
+            <h2 className="text-blue-500 font-bold py-4">
+              Week 3: Google My Business (Local SEO) & Developing a Social Media
+              Strategy
+            </h2>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 1: Google My Business
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Importance of Google My Business</li>
+              <li>Creating and Optimizing Google My Business Profile</li>
+            </ul>
+
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 2: Social Media Marketing
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Effective Social Media Marketing Steps</li>
+              <li>Benefits and Metrics of Social Media Marketing</li>
+              <li>Creating a Social Media Strategy</li>
+            </ul>
+
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 3: Facebook Marketing
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Effective Facebook Marketing Strategies</li>
+              <li>Facebook Ads Campaign</li>
+              <li>Using Facebook Analytics and Pixel</li>
+              <li>Generating Leads and Retargeting on Facebook</li>
+            </ul>
+
+            <h2 className="text-blue-500 font-bold py-4">
+              Week 4: Instagram Marketing & Email Marketing
+            </h2>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 1: Instagram Marketing
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Instagram Content Strategy</li>
+              <li>Best Practices and Tips for Instagram</li>
+              <li>Running Instagram Ad Campaigns</li>
+            </ul>
+
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 2: Email Marketing
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Importance of Email Marketing</li>
+              <li>Using Opt-Ins and Pop-Ups</li>
+              <li>Creating Campaigns with Mailchimp</li>
+              <li>Email Marketing Analytics</li>
+            </ul>
+
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 3: Pay-Per-Click: Google Ads
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Overview of Google Ads</li>
+              <li>Types of Google Ads Campaigns</li>
+              <li>Keyword Research and Campaign Setup</li>
+              <li>Google Video Ads (YouTube Marketing)</li>
+            </ul>
+
+            <h2 className="text-blue-500 font-bold py-4">
+              Week 5: TikTok Marketing & Copywriting
+            </h2>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 1: TikTok Marketing
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Importance of TikTok for Businesses</li>
+              <li>Creating a TikTok Marketing Strategy</li>
+              <li>Managing Brand Presence on TikTok</li>
+            </ul>
+
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 2: Copywriting
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>The "AIDA" Formula</li>
+              <li>Writing Magnetic Headlines</li>
+              <li>Creating Irresistible Calls to Action</li>
+            </ul>
+
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 3: LinkedIn Marketing
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Understanding Affiliate Marketing</li>
+              <li>Types and Pros/Cons of Affiliate Marketing</li>
+              <li>Monetizing through Affiliate Marketing</li>
+            </ul>
+
+            <h2 className="text-blue-500 font-bold py-4">Learning Outcomes</h2>
+            <p>By the end of this course, students will:</p>
+            <br />
+            <ul className="list-disc">
+              <li>Be able to set up and manage ad campaigns.</li>
+              <li>Build a career as a Digital Marketing Professional.</li>
+              <li>Develop strategies as a Digital Marketing Strategist.</li>
               <li>
-                Learning of Conversion Rate Optimization including AB / Split
-                Testing (s)
+                Understand methodologies, tools, and technologies in Digital
+                Marketing.
               </li>
               <li>
-                Learning of custom dashboard / segments/ reports generation
+                Grasp essential philosophies and practices of marketing and
+                digital marketing technologies.
               </li>
             </ul>
           </div>
@@ -82,4 +202,4 @@ const DigitalMarketingSyllabus = () => {
   );
 };
 
-export default DigitalMarketingSyllabus;
+export default DataAnalytics;
