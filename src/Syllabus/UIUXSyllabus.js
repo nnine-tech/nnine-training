7bf49754c7681864c7bd43a458a3b00812c7dfa1
ngimatendi <ngimatendi12@gmail.com>
2024-06-20 00:02:04 +0545

    feat: Digital Marketing, Graphic Designing and UIUX Syllabus Updated

---
+++ src/Syllabus/UIUXSyllabus.js
@@ -0,0 +1,299 @@
+import React from "react";
+
+const UIUXDesign = () => {
+  return (
+    <>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="bg-blue-300 min-h-screen p-8">
+        <div className="max-w-3xl mx-auto bg-white p-8 rounded-lg shadow-md">
+          <div>
+            <h1 className="text-blue-500 text-3xl font-bold py-8 text-center">
+              UI/UX Designing Syllabus
+            </h1>
+            <h1 className="text-blue-500 text-2xl font-bold">
+              Course Information
+            </h1>
+            <h1 className="text-blue-500 text-2xl font-bold">
+              Nature of the Course:
+              <span className="font-normal">Theory + Practical</span>
+            </h1>
+            <h1 className="text-blue-500 text-2xl font-bold">
+              Course Duration: <span className="font-normal">40 Hours</span>
+            </h1>
+
+            <h1 className="text-blue-500 text-2xl font-bold py-4">
+              Course Objectives
+            </h1>
+            <p>
+              The UI/UX Design course takes a design-centric approach to user
+              interface and user experience design, providing practical,
+              skill-based training focused on visual communications rather than
+              marketing or programming alone. You will learn all aspects of the
+              UI/UX development process, from user research to establishing a
+              project's strategy, scope, and information architecture, to
+              generating sitemaps and wireframes.
+            </p>
+
+            <h1 className="text-blue-500 text-2xl font-bold py-4">
+              Required Textbooks
+            </h1>
+            <ul className="list-disc ml-8">
+              <li>Joel Marsh, “UX for Beginners”, O’Reilly</li>
+              <li>
+                Jenifer Tidwell, Charles Brewer, and Aynne Valencia, “Designing
+                Interfaces”, O’Reilly
+              </li>
+            </ul>
+
+            <h1 className="text-blue-500 text-2xl font-bold py-4">
+              Course Outline
+            </h1>
+            <h2 className="text-blue-500 text-lg font-bold py-4">
+              Unit 1: Design Fundamentals - User Interface
+            </h2>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 1: Introduction to User Interface
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Relationship between UI and UX</li>
+              <li>UI vs. Frontend</li>
+              <li>Roles in UI/UX</li>
+              <li>Interface Conventions</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 2: Historical Overview of Interface Design
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Interface Conventions: Theory and Application</li>
+              <li>The Golden Rule</li>
+              <li>Approaches to Screen-Based UI</li>
+              <li>Formal Elements of an Interface</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 3: Design before Design
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Template vs. Content</li>
+              <li>Aesthetics and Functionality</li>
+              <li>Look and Feel</li>
+              <li>Language as a Design Tool</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 4: Colors and Shapes
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Imagery, Typography, and Icons</li>
+              <li>Points of Interaction</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 5: Buttons
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Designing Better Buttons</li>
+              <li>States and Changes</li>
+              <li>Composing Interaction</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 6: Foundations of User Experience (UX)
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Speed and Style</li>
+              <li>Composition and Structure</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 7: The Basics of User Experience Design
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Scopes of User Experience</li>
+              <li>The Product Development Life Cycle</li>
+            </ul>
+            <h2 className="text-blue-500 text-lg font-bold py-4">
+              Unit 2: Common Terms, Tools, and Frameworks in UX Design
+            </h2>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 1: Defining User, End-User, and User Experience
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Prioritizing the User</li>
+              <li>Key Frameworks in UX Design</li>
+              <li>Designing Across Platforms</li>
+              <li>Design for Accessibility</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 2: Design Sprints
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Introduction to Design Sprints</li>
+              <li>Phases and Benefits of Design Sprints</li>
+              <li>Planning Design Sprints</li>
+              <li>Design Sprint Retrospectives</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 3: Figma: The Interface Design Tool
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Introduction to Figma</li>
+              <li>Web Apps vs. Native Apps</li>
+              <li>Figma User Interface, Community, Plugins</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 4: Creating New Files and Designing on a Grid
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Importing and Cropping Photos</li>
+              <li>Setting Up Frames</li>
+              <li>Adding Text and Colored Backgrounds</li>
+              <li>Importing and Modifying Vector Graphics</li>
+              <li>Aligning and Distributing Layers</li>
+            </ul>
+            <h2 className="text-blue-500 font-bold py-4">
+              Unit 3: Advanced Figma Techniques
+            </h2>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 1: Text Styles
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Local vs. Web Fonts</li>
+              <li>Handling Missing Fonts</li>
+              <li>Creating, Editing, and Organizing Text Styles</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 2: Components
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Creating and Editing Components</li>
+              <li>
+                Overriding Content in One Instance vs. Globally Updating All
+                Components
+              </li>
+              <li>Detaching from a Component</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 3: Auto Layout
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Using Auto Layout</li>
+              <li>Constraints and Resizing</li>
+              <li>Spacing, Rearranging, Adding Items with Auto Layout</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 4: Variants
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Creating and Using Component Variants</li>
+              <li>Variant Properties and Values</li>
+              <li>Boolean Values</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 5: Hovers and Overlays
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Adding a Hover State to a Button</li>
+              <li>Opening a Pop-Up</li>
+              <li>Linking between Frames</li>
+              <li>Previewing the Prototype</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 6: Figma Animation: Intro to Start Animate
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Basics of Smart Animate</li>
+              <li>Different Kinds of Easing</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 7: Making Links that Scroll Up/Down a Page
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Making Navbar Fixed to the Screen</li>
+              <li>Adjusting Scrollable Area within a Page</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 8: Adding Hyperlinks
+            </h3>
+            <h2 className="text-blue-500 font-bold py-4">
+              Unit 4: Exporting and HTML Basics
+            </h2>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 1: Exporting Assets for Web: JPEG, PNG, and SVG
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Exporting Individual Assets</li>
+              <li>Exporting Frames</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 2: Sharing Figma Files
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Commenting on Shared Files</li>
+              <li>Viewing a Prototype for User Testing</li>
+              <li>Specs for Developers</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 3: Hypertext Markup Language (HTML)
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Introduction to HTML Elements and Attributes</li>
+              <li>HTML Headings and Paragraphs</li>
+              <li>HTML Styles and Formatting</li>
+              <li>HTML Quotation, Comments, and Colors</li>
+              <li>HTML Links and Images</li>
+              <li>HTML Tables and Lists</li>
+              <li>HTML Block and Inline Elements</li>
+              <li>HTML Responsive Design</li>
+              <li>HTML Forms</li>
+            </ul>
+            <h3 className="text-blue-500 font-semibold py-2">
+              Lesson 4: Cascading Style Sheets (CSS)
+            </h3>
+            <ul className="list-disc ml-8">
+              <li>Introduction to CSS Rulesets and Selectors</li>
+              <li>CSS Declarations, Values, and Units</li>
+              <li>CSS Box Model and Functions</li>
+              <li>CSS Animations and Media Queries</li>
+              <li>CSS Library</li>
+            </ul>
+            <h1 className="text-blue-500 text-2xl font-bold py-4">
+              Learning Outcomes
+            </h1>
+            <ul className="list-disc ml-8">
+              <li>
+                Using Integrated Development Environments (IDEs) such as Adobe
+                Dreamweaver and Adobe Air Interface, create a web page.
+              </li>
+              <li>
+                Using Flash MX, create animated web content and applications.
+              </li>
+              <li>
+                Animate lights and cameras, dynamics page, video effects in 3DS
+                MAX, polygon modeling, mesh modeling, patch modeling, and NURBS
+                modeling.
+              </li>
+              <li>
+                Perform character animation, free transform animation, motion
+                capture files transition creation, mesh deformation with skin
+                and physique modifiers, crowd animation with system delegate
+                objects, cognitive controller.
+              </li>
+              <li>
+                Work with analog and digital sound, sound recording, processing,
+                and effects.
+              </li>
+            </ul>
+            <h1 className="text-blue-500 text-2xl font-bold py-4">Labs</h1>
+            <p>
+              Lab assignments will focus on the practice and mastery of content
+              covered in lectures and introduce critical and fundamental
+              problem-solving techniques to the students.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UIUXDesign;
---
+++ src/app/Syllabus/GraphicDesignSyllabus/page.js
@@ -0,0 +1,12 @@
+import GraphicDesignSyllabus from "@/Syllabus/GraphicDesignSyllabus";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <GraphicDesignSyllabus />
+    </div>
+  );
+};
+
+export default page;
