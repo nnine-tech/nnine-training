f184c35ac464931185b5e17f5194527b05a2ce1a
Shresthanirjala <nirjalas437@gmail.com>
2024-08-29 11:15:48 +0545

    feat: syllabus updated

--- src/components/DataScienceAndMLClassType.js
+++ src/components/DataScienceAndMLClassType.js
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import DevopsSchedule from "./DevopsSchedule";
 import DataScienceAndMLClassSchedule from "./DataScienceAndMLClassSchedule";
 
 const DataScienceAndMLClassType = () => {
--- src/components/DataScienceAndMLSyllabus1.js
+++ src/components/DataScienceAndMLSyllabus1.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import DataScienceAndMLClassType from "./DataScienceAndMLClassTYpe";
+import DataScienceAndMLClassType from "./DataScienceAndMLClassType";
 
 
 const DataScienceAndMLSyllabus1 = () => {
