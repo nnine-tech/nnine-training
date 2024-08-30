ce02cad733a1ae32a40a64961ca1c9297c786804
PriyaDev38 <oedzpriya1@gmail.com>
2024-08-30 13:21:25 +0545

    updated

--- src/components/EthicalHackingIntro.js
+++ src/components/EthicalHackingIntro.js
@@ -40,7 +40,7 @@ const EthicalHackingIntro = () => {
               </div>
               <div className="flex items-center gap-2">
                 <img src="/duration.svg" alt="Duration" />
-                <h2 className="text-[#FFFFFF]">Duration: 2hrs</h2>
+                <h2 className="text-[#FFFFFF]">Duration: 90h</h2>
               </div>
               <div className="flex items-center gap-2">
                 <img src="/career.svg" alt="Career" />
