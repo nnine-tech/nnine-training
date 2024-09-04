9d0acf09b9ed9ff93581fa3feb7914f28e8d4d5e
Shresthanirjala <nirjalas437@gmail.com>
2024-09-04 15:23:56 +0545

    feat:updated

--- src/components/RedHatOpenShiftSyllabus1.js
+++ src/components/RedHatOpenShiftSyllabus1.js
@@ -118,59 +118,60 @@ const RedHatOpenShiftSyllabus1 = () => {
   
   
   
-  
   return (
     <div className="w-full flex flex-col md:flex-row sm:flex-row sm:mt-4">
       <div className="w-[80%] flex flex-col mt-14">
-      <div className="w-full">
-  <h1 className="font-bold text-3xl ml-8 p-[10px] font-poppins mb-[37px] text-[#003366]">
-    Course Syllabus
-  </h1>
-  {syllabusSections.map((section, index) => (
-    <div key={index} className={`relative mx-auto w-[400px] md:w-full`}>
-      <div
-        className={`border-2 px-4 border-[#004AAD] border-opacity-[5%] transition-all   ${
-          !openDropdowns[section.title] && index !== syllabusSections.length - 1
-            ? "border-b-5"
-            : ""
-        }`}
-        style={{ width: '100%', maxWidth: '800px', minWidth: '300px' }} // Setting max and min widths
-      >
-        <div
-          className="flex items-center p-[10px]"
-          onClick={() => toggleDropdown(section.title)}
-        >
-          <button className="dropdown-button mr-2">
-            <img
-              src="/dropdown.svg"
-              alt="Toggle Dropdown"
-              className="mr-0 w-[11px] h-[14px]"
-            />
-          </button>
-          <span className="font-semibold font-poppins">
-            {section.title}
-          </span>
-          <span className="hidden md:block text-right ml-auto font-poppins">
-            {section.info}
-          </span>
+        <div className="w-full">
+          <h1 className="font-bold text-3xl ml-8 p-[10px] font-poppins mb-[37px] text-[#003366]">
+            Course Syllabus
+          </h1>
+          {syllabusSections.map((section, index) => (
+            <div
+              key={index}
+              className={`relative w-full max-w-[800px]`}
+            >
+              <div
+                className={`border-2 px-4 border-[#004AAD] border-opacity-[5%] transition-all ${
+                  !openDropdowns[section.title] &&
+                  index !== syllabusSections.length - 1
+                    ? "border-b-5"
+                    : ""
+                }`}
+              >
+                <div
+                  className="flex items-center p-[10px] cursor-pointer"
+                  onClick={() => toggleDropdown(section.title)}
+                >
+                  <button className="dropdown-button mr-2">
+                    <img
+                      src="/dropdown.svg"
+                      alt="Toggle Dropdown"
+                      className="mr-0 w-[11px] h-[14px]"
+                    />
+                  </button>
+                  <span className="font-semibold font-poppins">
+                    {section.title}
+                  </span>
+                  <span className="hidden md:block text-right ml-auto font-poppins">
+                    {section.info}
+                  </span>
+                </div>
+              </div>
+              {openDropdowns[section.title] && (
+                <div className="w-full border border-[#004AAD] border-opacity-[5%] z-10 mt-1 mr-4">
+                  <ul className="p-2">
+                    {section.lectures.map((lecture, idx) => (
+                      <li key={idx} className="py-1">
+                        <span className="mx-1">•</span>
+                        {lecture}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          ))}
         </div>
-      </div>
-      {openDropdowns[section.title] && (
-        <div className="relative w-full border border-[#004AAD] border-opacity-[5%] z-10 mt-1 mr-4">
-          <ul className="p-2">
-            {section.lectures.map((lecture, idx) => (
-              <li key={idx} className="py-1">
-                <span className="mx-1">•</span>
-                {lecture}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
-  ))}
-</div>
-
       </div>
       <GraphicDesignClassType/>
     </div>
