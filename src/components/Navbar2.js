c7069d53005042a7a396569e3d380a7e0f1217f2
Shresthanirjala <nirjalas437@gmail.com>
2024-09-19 11:00:51 +0545

    feat: navbar

---
+++ src/components/Navbar2.js
@@ -0,0 +1,121 @@
+"use client";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [top, setTop] = useState(true);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const scrollHandler = () => {
+      window.pageYOffset > 10 ? setTop(false) : setTop(true);
+    };
+    window.addEventListener("scroll", scrollHandler);
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, []);
+
+  return (
+    <nav
+      className={`fixed top-0 w-full z-30 transition duration-300 ease-in-out mb-16 rounded-full bg-[#E9E9E9] shadow-lg`}
+    >
+      <div className="flex items-center justify-between py-1 px-6">
+        {/* Logo */}
+        <Link href="/">
+          <div className="flex items-center">
+            <img
+              src="/logo2.svg"
+              alt="N9 Solution Logo"
+              className="h-10 w-auto"
+            />
+          </div>
+        </Link>
+
+        {/* Hamburger button (for mobile view) */}
+        <div className="group flex items-center">
+          <button
+            className="p-2 rounded-lg lg:hidden text-blue-900"
+            onClick={handleClick}
+          >
+            <svg
+              className="h-6 w-6 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              {isOpen ? (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+                />
+              ) : (
+                <path
+                  fillRule="evenodd"
+                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2z"
+                />
+              )}
+            </svg>
+          </button>
+
+          {/* Navigation Links */}
+          <ul
+            className={`lg:flex lg:flex-row lg:space-x-6 lg:p-5 ${
+              isOpen ? "block" : "hidden"
+            } absolute top-16 right-0 bg-white lg:relative lg:bg-transparent lg:top-auto lg:right-auto lg:w-auto`}
+          >
+            <li>
+              <Link
+                href="/Popularcourses"
+                className="font-bold text-[#6175AD] hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl"
+              >
+                Courses
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/Trainer"
+                className="font-bold text-[#6175AD] hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl"
+              >
+                Trainer
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/Career"
+                className="font-bold text-[#6175AD] hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl"
+              >
+                Career
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/About"
+                className="font-bold text-[#6175AD] hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/Contact"
+                className="font-bold text-[#6175AD] hover:bg-blue-100 inline-block md:inline-flex items-center justify-center w-auto px-4 py-2 rounded-xl"
+              >
+                Contact Us
+              </Link>
+            </li>
+            <li>
+              <button className="flex justify-center border border-[#050599] rounded-full p-2 w-auto font-semibold  text-[#0F0FAE] lg:ml-20 lg:w-[149px]">
+                Get started
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
