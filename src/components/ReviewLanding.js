b2c23857f5ecf98d6ea4894868fb7e4212ad2d88
Shresthanirjala <nirjalas437@gmail.com>
2024-09-20 11:36:48 +0545

    feat: landing page updated

---
+++ src/components/ReviewLanding.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const ReviewLanding = () => {
+  return (
+    <div className='w-full h-[500px] mt-8'>
+       <h1 className='flex justify-center items-center font-'>What Our students says about us</h1>
+      <div className='flex flex-row gap-8 bg-violet-700 ml-10 mr-10 justify-center '>
+       
+       
+       <div className='bg-[#F4F4F4] w-[447px] h-[360px] rounded-2xl shadow-2xl '>hi</div>
+       <div className='w-[447px] h-[360px] bg-[#F4F4F4]  rounded-2xl shadow-2xl'> hi</div>
+       <div className='w-[447px] h-[360px] bg-[#F4F4F4]  rounded-2xl shadow-2xl'>jello</div>
+
+
+
+      </div>
+
+
+   
+    </div>
+  )
+}
+
+export default ReviewLanding
