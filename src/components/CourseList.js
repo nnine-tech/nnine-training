d4523d8d9a5357d1ced8ef086eaf2a1d2fcd4a33
Shresthanirjala <nirjalas437@gmail.com>
2024-09-24 12:42:41 +0545

    feat

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -5,19 +5,12 @@ import React from "react";
 import Card from "./Card";
 import Card2 from "./Card2";
 
-
 const CourseList = ({ courseData }) => {
   return (
     <>
-    
-        
-
       <div className="flex flex-wrap justify-center mt-28">
         {courseData.map((card, index) => (
-          <Card2 key={index} {...card} 
-        
-          />
-          
+          <Card2 key={index} {...card} />
         ))}
       </div>
     </>
