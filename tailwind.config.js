8935f4945620a2ead248e0bf75c5d0825c968933
Shresthanirjala <nirjalas437@gmail.com>
2024-11-11 16:12:47 +0545

    feat: trainers page updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,8 @@ module.exports = {
         "Montserrat":"Montserrat",
         robotoSlab: ['Roboto Slab', 'serif'],
         "Quicksand": "Quicksand",
-        "Laila": "Laila"
+        "Laila": "Laila",
+        "Lexend": "Lexend"
       },
       boxShadow: {
         'bottom': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
