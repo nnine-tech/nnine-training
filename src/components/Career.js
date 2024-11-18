6c8acf44ef90fc1f7a3e526e4ed1116c3c3b08de
madhavbanjade <madhavbanjade005@gmail.com>
2024-11-18 16:29:36 +0545

    feat: careerUpdated

--- src/components/Career.js
+++ src/components/Career.js
@@ -3,24 +3,17 @@ import Link from "next/link";
 
 const Career = () => {
   const jobPositions = [
+    ,
     {
       title: "Product Designer",
       description:
         "We're looking for a mid-level product designer to join our team.",
       location: "Hybrid",
       type: "Full-time",
-      applyLink: "/ProductDesigner",
+      applyLink: "ProductDesigner",
     },
     {
-      title: "Product Designer",
-      description:
-        "We're looking for a mid-level product designer to join our team.",
-      location: "Hybrid",
-      type: "Full-time",
-      applyLink: "/product-designer",
-    },
-    {
-      title: "Product Designer",
+      title: "Frontend Developer",
       description:
         "We're looking for a mid-level product designer to join our team.",
       location: "Hybrid",
@@ -28,7 +21,7 @@ const Career = () => {
       applyLink: "/product-designer",
     },
     {
-      title: "Product Designer",
+      title: "Backend Developer",
       description:
         "We're looking for a mid-level product designer to join our team.",
       location: "Hybrid",
@@ -36,7 +29,7 @@ const Career = () => {
       applyLink: "/product-designer",
     },
     {
-      title: "Product Designer",
+      title: "Quality Assurance",
       description:
         "We're looking for a mid-level product designer to join our team.",
       location: "Hybrid",
@@ -44,7 +37,7 @@ const Career = () => {
       applyLink: "/product-designer",
     },
     {
-      title: "Product Designer",
+      title: "HR Manager",
       description:
         "We're looking for a mid-level product designer to join our team.",
       location: "Hybrid",
@@ -52,7 +45,7 @@ const Career = () => {
       applyLink: "/product-designer",
     },
     {
-      title: "Product Designer",
+      title: "Internship",
       description:
         "We're looking for a mid-level product designer to join our team.",
       location: "Hybrid",
@@ -69,7 +62,7 @@ const Career = () => {
           alt=""
           className="w-full h-[300px] sm:h-[500px] lg:h-[800px] object-cover"
         />
-        <div className="absolute inset-0 flex flex-col gap-4 sm:gap-6 lg:gap-6 h-auto sm:h-[300px] w-full sm:w-[90%] lg:w-[1000px] justify-center items-center mx-auto mt-56 sm:mt-40 lg:mt-80 text-center px-4">
+        <div className="absolute inset-0 flex flex-col gap-4 sm:gap-6 lg:gap-6 h-auto sm:h-[300px] w-full sm:w-[90%] lg:w-[1000px] justify-center items-center mx-auto mt-56 sm:mt-40 lg:mt-50 text-center px-4">
           <h1 className="text-white font-Quicksand text-2xl sm:text-4xl lg:text-6xl font-bold mt-20">
             Join <span className="text-[#FFCF59]">Our</span> Team
           </h1>
@@ -80,60 +73,26 @@ const Career = () => {
             <br className="hidden sm:block" />
             ownership and responsibility
           </p>
-          <div className="flex justify-center w-full px-4 sm:px-0">
-            <div className="bg-white shadow-2xl border-violet-300 lg:ml-60 h-[200px] w-full sm:w-[300px] rounded-lg p-3  flex flex-col justify-between transition-transform duration-300 ease-in-out transform hover:scale-105 ">
-              <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center ">
-                <h1 className="font-Quicksand text-[#000000] text-base sm:text-lg font-bold">
-                  {jobPositions[0].title}
-                </h1>
-                <Link href={jobPositions[0].applyLink}>
-                  <h2 className="font-Quicksand text-[#0060FF] flex items-center font-semibold cursor-pointer">
-                    Apply
-                    <img
-                      src="right-up (1).png"
-                      alt="Apply"
-                      className="w-4 sm:w-[22px] h-4 sm:h-[22px] ml-2"
-                    />
-                  </h2>
-                </Link>
-              </div>
-              <p className="text-sm font-medium font-Quicksand pt-2">
-                {jobPositions[0].description}
-              </p>
-              <div className="flex flex-wrap gap-2 sm:gap-4 pt-4 mb-40">
-                <span className="h-auto sm:h-[38px] w-auto sm:w-[108px] border border-slate-950 rounded-full text-xs flex items-center gap-2 px-2 sm:p-3">
-                  <img
-                    src="loc.png"
-                    alt={jobPositions[0].location}
-                    className="w-4 sm:w-[22px] h-4 sm:h-[22px]"
-                  />
-                  {jobPositions[0].location}
-                </span>
-                <span className="h-auto sm:h-[38px] w-auto sm:w-[108px] border border-slate-950 rounded-full text-xs flex items-center gap-2 px-2 sm:p-3">
-                  <img
-                    src="clock.png"
-                    alt={jobPositions[0].type}
-                    className="w-4 sm:w-[22px] h-4 sm:h-[22px]"
-                  />
-                  {jobPositions[0].type}
-                </span>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
 
-      <div className="flex flex-wrap justify-center gap-4 sm:gap-[32px] mx-auto max-w-5xl px-4 mt-[200px] sm:mt-8 pb-28">
+      <div className="flex flex-wrap justify-center mx-auto max-w-6xl px-4 pb-28 relative gap-x-8 gap-y-10 -mt-[200px]">
         {jobPositions.slice(1).map((job, index) => (
           <div
             key={index}
-            className="bg-white shadow-2xl border-violet-300 h-auto sm:h-[180px] w-full sm:w-[300px] rounded-lg p-3 flex flex-col justify-between transition-transform duration-300 ease-in-out transform hover:scale-105"
+            className={`bg-white shadow-2xl border-violet-300 h-auto sm:h-[180px] w-full sm:w-[300px] md:w-[350px] lg:w-[320px] xl:w-[340px] rounded-lg p-3 flex flex-col justify-between transition-transform duration-300 ease-in-out transform hover:scale-105 ${
+              index % 3 === 0
+                ? "translate-y-0"
+                : index % 3 === 1
+                ? "translate-y-[40px]"
+                : "translate-y-[80px]"
+            }`}
           >
             <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center">
               <h1 className="font-Quicksand text-[#000000] text-base sm:text-lg font-bold">
                 {job.title}
               </h1>
-              <Link href={job.applyLink}>
+              <Link href={job.applyLink} key={index}>
                 <h2 className="font-Quicksand text-[#0060FF] flex items-center font-semibold cursor-pointer">
                   Apply
                   <img
