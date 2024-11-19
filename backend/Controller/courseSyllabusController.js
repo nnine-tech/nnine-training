0cd91daddcf61d00d474a8c76f3da83a6795be78
bigyanrai <9813199661now@gmail.com>
2024-11-19 16:46:36 +0545

    feat: courseSyllabusRoute's createSyllabus updated as nested route

--- backend/Controller/courseSyllabusController.js
+++ backend/Controller/courseSyllabusController.js
@@ -1,7 +1,15 @@
 const CourseSyllabus = require("../Model/coursesyllabusModel");
 
+exports.setCourseId = (req, res, next) => {
+  //allow nested routes
+  if (!req.body.course) req.body.course = req.params.courseId;
+  console.log(req.body);
+  next();
+};
+
 exports.createSyllabus = async (req, res, next) => {
   const syllabus = await CourseSyllabus.create(req.body);
+  console.log(syllabus);
   res.status(200).json({
     status: "success",
     data: {
