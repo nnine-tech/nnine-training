568c96ab754202a26f27fc6ae41f50c1c263ab2d
Aarayn10-sudo <lazyfox916@gmail.com>
2024-11-19 12:00:51 +0545

    fix : Student API

--- backend/Controller/studentController.js
+++ backend/Controller/studentController.js
@@ -14,11 +14,68 @@ exports.addStudentController = async (req, res, next) => {
   }
 };
 
-exports.getFeeController = async (req, res, next) => {
+exports.getStudentController = async (req, res, next) => {
   try {
     const result = await Student.find();
     res.status(200).json({
       message: "Fee details fetched successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+
+exports.updateStudentController = async (req, res, next) => {
+  try {
+    const result = await Student.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json({
+      message: "Student updated successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+
+exports.searchStudent = async (req, res, next) => {
+  try {
+    const item = req.query.item;
+    const pipeline = [];
+    pipeline.push({
+      $match: {
+        $or: [
+          { firstName: { $regex: item, $options: "i" } },
+          { lastName: { $regex: item, $options: "i" } },
+          { department: { $regex: item, $options: "i" } },
+          { email: { $regex: item, $options: "i" } },
+          { courseTaken: { $regex: item, $options: "i" } },
+        ],
+      },
+    });
+    const result = await Student.aggregate(pipeline);
+    res.status(200).json({
+      message: "Students found successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+
+exports.deleteStudent = async (req, res, next) => {
+  try {
+    const result = await Student.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "Student deleted successfully",
       data: result,
     });
   } catch (error) {
--- backend/Routes/studentRoute.js
+++ backend/Routes/studentRoute.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const {
   addStudentController,
-  getFeeController,
+  getStudentController,
+  updateStudentController,
+  searchStudent,
+  deleteStudent,
 } = require("../Controller/studentController");
 const router = express.Router();
 
 router.route("/add").post(addStudentController);
-router.route("/readAll").get(getFeeController);
+router.route("/readAll").get(getStudentController);
+router.route("/search").get(searchStudent);
+router.route("/update/:id").patch(updateStudentController);
+router.route("/delete/:id").delete(deleteStudent);
 
 module.exports = router;
