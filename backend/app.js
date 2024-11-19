1f7eeb0788093c7172d03182892216f1a5a439da
bigyanrai <9813199661now@gmail.com>
2024-11-19 12:16:17 +0545

    feat: course model and syllabus model done

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,15 @@ process.on("uncaughtException", (err) => {
 const express = require("express");
 const app = express();
 const courseRoute = require("./Routes/courseRoute");
+const courseSyllabusRoute = require("./Routes/courseSyllabusRoute");
+
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+//MIDDLEWARES
+app.use(express.json());
 
 //BACKEND ROUTE
 app.use("/api/v1/courses", courseRoute);
+app.use("/api/v1/syllabus", courseSyllabusRoute);
 
 module.exports = app;
