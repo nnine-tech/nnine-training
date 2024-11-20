457a729dbfe50b79e2c794477fa5cbc339c564ab
bigyanrai <9813199661now@gmail.com>
2024-11-20 19:13:00 +0545

    feat: course controller is updated

--- backend/Controller/courseSyllabusController.js
+++ backend/Controller/courseSyllabusController.js
@@ -8,22 +8,94 @@ exports.setCourseId = (req, res, next) => {
 };
 
 exports.createSyllabus = async (req, res, next) => {
-  const syllabus = await CourseSyllabus.create(req.body);
-  console.log(syllabus);
-  res.status(200).json({
-    status: "success",
-    data: {
-      data: syllabus,
-    },
-  });
+  try {
+    const syllabus = await CourseSyllabus.create(req.body);
+    console.log(syllabus);
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: syllabus,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failure",
+      error: error,
+    });
+  }
+};
+
+exports.getAllSyllabus = async (req, res, next) => {
+  try {
+    const syllabus = await CourseSyllabus.find();
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: syllabus,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failure",
+      error: error,
+    });
+  }
 };
 
 exports.getSyllabus = async (req, res, next) => {
-  const syllabus = await CourseSyllabus.find();
-  res.status(200).json({
-    status: "success",
-    data: {
-      data: syllabus,
-    },
-  });
+  try {
+    const syllabus = await CourseSyllabus.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: syllabus,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failure",
+      error: error,
+    });
+  }
+};
+
+exports.updateSyllabus = async (req, res, next) => {
+  try {
+    const syllabus = await CourseSyllabus.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        //RUN VALIDATOR IS TURNED OFF FOR NOW BUT MAY REQUIRE TO BE TURNED OFF LATER
+        // runValidator: true,
+      }
+    );
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: syllabus,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failure",
+      error: error,
+    });
+  }
+};
+exports.deleteSyllabus = async (req, res, next) => {
+  try {
+    const syllabus = await CourseSyllabus.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: syllabus,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failure",
+      error: error,
+    });
+  }
 };
