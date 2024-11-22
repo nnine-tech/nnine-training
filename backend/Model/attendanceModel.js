5d90e952f62ba3b5b8e81cdb88c57a3aff0e777b
Prabin70 <xthaprabin125@gmail.com>
2024-11-22 12:55:00 +0545

    feat:attendanceAPI

---
+++ backend/Model/attendanceModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const attendanceSchema = new mongoose.Schema({
+
+    students_id: {
+        type: String,
+        required: [true, "studentsName is required"],
+
+    },
+    checkedIn: {
+        type: String,
+        required: [true, "checkedIn is required"],
+    },
+    checkout: {
+        type: String,
+        required: [true, "checkout is required"],
+    },
+
+    status: {
+        type: Boolean,
+        required: [true, "status is required"]
+
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+
+
+});
+
+let Attendance = mongoose.model('Attendance', attendanceSchema);
+
+
+
+module.exports = Attendance;
