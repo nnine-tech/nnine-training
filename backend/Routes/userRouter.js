8a3faaf229cec2b277d9098fc2b0de0bdd87b068
madhavbanjade <madhavbanjade005@gmail.com>
2024-11-27 12:37:55 +0545

    feat:update

--- backend/Routes/userRouter.js
+++ backend/Routes/userRouter.js
@@ -19,7 +19,7 @@ userRouter.route("/update/:id").patch(updateUserController);
 
 userRouter.route("/delete/:id").delete(deleteUserController);
 
-  module.exports = {userRouter};
+  module.exports = userRouter;
 
 
 
